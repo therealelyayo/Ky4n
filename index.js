c5c633b1d4fab1386f070710c640cae5de8414fb
aqu4x0 <aquax0@protonmail.com>
2022-03-18 22:24:09 -0400

    ⚠️ - Verificacion de configuracion

--- index.js
+++ index.js
@@ -14,52 +14,62 @@ const express = require("express"),
 /** @description: Ahora la estructura principal del servidor: */
 
 const index = (port) => {
-  connect(Auth["MongoURI"], {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-    .then(() => {
-      console.log(`[MongoDB] :: Conectado exitosamente a la base de datos.`);
+  if (Auth["Password"] == null) {
+    console.log("`Password` no esta rellenado en: settings.json");
+    process.exit(1);
+  } else if (Auth["MongoURI"] == null) {
+    console.log("`MongoURI` no esta rellenado en: settings.json");
+    process.exit(1);
+  } else {
+    connect(Auth["MongoURI"], {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
-    .catch((err) => {
-      console.log(`[MongoDB] :: Error al conectar a la base de datos: ${err}`);
+      .then(() => {
+        console.log(`[MongoDB] :: Conectado exitosamente a la base de datos.`);
+      })
+      .catch((err) => {
+        console.log(
+          `[MongoDB] :: Error al conectar a la base de datos: ${err}`
+        );
+      });
+    APP.use(
+      session({
+        secret: "secret",
+        resave: false,
+        saveUninitialized: false,
+      })
+    );
+    APP.set("views", join(__dirname, "views"));
+    APP.set("view engine", "ejs");
+    APP.use(express.static("public"));
+    APP.use(json());
+
+    APP.use(
+      urlencoded({
+        extended: false,
+      })
+    );
+
+    APP.use((request, response, next) => {
+      request.pass = Auth["Password"];
+      next();
     });
-  APP.use(
-    session({
-      secret: "secret",
-      resave: false,
-      saveUninitialized: false,
-    })
-  );
-  APP.set("views", join(__dirname, "views"));
-  APP.set("view engine", "ejs");
-  APP.use(express.static("public"));
-  APP.use(json());
 
-  APP.use(
-    urlencoded({
-      extended: false,
-    })
-  );
+    APP.use("/", require("./src/router/auth"));
+    APP.use("/v1/api", require("./src/router/API"));
 
-  APP.use((request, response, next) => {
-    request.pass = Auth["Password"];
-    next();
-  });
-
-  APP.use("/", require("./src/router/auth"));
-  APP.use("/v1/api", require("./src/router/API"));
-
-  APP.get("*", async (request, response) => {
-    response.status(404).render("404", {
-      title: "Ky4n | 404 Error",
+    APP.get("*", async (request, response) => {
+      response.status(404).render("404", {
+        title: "Ky4n | 404 Error",
+      });
     });
-  });
 
-  APP.listen(port, () => {
-    console.clear();
-    console.log(`[SERVER] :: Servidor iniciado en el puerto ${port}`);
-  });
+    APP.listen(port, () => {
+      console.clear();
+      console.log(`[SERVER] :: Servidor iniciado en el puerto ${port}`);
+    });
+  }
 };
 
 /**
